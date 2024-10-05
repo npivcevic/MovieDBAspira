6c1fd8ad870ee09d6c5c65c34466a85539988462
Nikola Pivcevic <nikola.pivcevic@outlook.com>
2024-10-05 13:29:45 +0200

    Scaffold new MoviesController using EntityFramework

--- MovieDBAspiraWebAPI/Controllers/MoviesController.cs
+++ MovieDBAspiraWebAPI/Controllers/MoviesController.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using MovieDBAspiraWebAPI.Models;
-
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace MovieDBAspiraWebAPI.Controllers
 {
@@ -9,59 +13,95 @@ namespace MovieDBAspiraWebAPI.Controllers
     [ApiController]
     public class MoviesController : ControllerBase
     {
-        private static List<Movie> movies = [
-            new Movie() {
-                Id = Guid.Parse("99d30d32-5b1f-486b-9dfd-151ecec1b5f0"),
-                Title = "Ironman",
-                ReleaseYear = 2006,
-            },
-            new Movie() {
-                Id = Guid.Parse("b8344da8-0864-4b24-b9c4-88d10e6635af"),
-                Title = "Spiderman",
-                ReleaseYear = 2008,
-            }
-            ];
+        private readonly DataContext _context;
 
-        // GET: api/<MoviesController>
+        public MoviesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Movies
         [HttpGet]
-        public IEnumerable<Movie> Get()
+        public async Task<ActionResult<IEnumerable<Movie>>> GetMovies()
         {
-            return movies;
+            return await _context.Movies.ToListAsync();
         }
 
-        // GET api/<MoviesController>/5
+        // GET: api/Movies/5
         [HttpGet("{id}")]
-        public Movie Get(Guid id)
+        public async Task<ActionResult<Movie>> GetMovie(Guid id)
         {
-            return movies.First(movie => movie.Id == id);
-        }
+            var movie = await _context.Movies.FindAsync(id);
 
-        // POST api/<MoviesController>
-        [HttpPost]
-        public void Post([FromBody] Movie value)
-        {
-            movies.Add(value);
-        }
-
-        // PUT api/<MoviesController>/5
-        [HttpPut("{id}")]
-        public void Put(Guid id, [FromBody] Movie updatedMovie)
-        {
-            int index = movies.FindIndex(movie => movie.Id == id);
-
-            if (index == -1)
+            if (movie == null)
             {
-                return;
+                return NotFound();
             }
 
-            movies[index] = updatedMovie;
+            return movie;
         }
 
-        // DELETE api/<MoviesController>/5
-        [HttpDelete("{id}")]
-        public void Delete(Guid id)
+        // PUT: api/Movies/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMovie(Guid id, Movie movie)
         {
-            movies.RemoveAll(movie => movie.Id == id);
+            if (id != movie.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(movie).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MovieExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Movies
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Movie>> PostMovie(Movie movie)
+        {
+            _context.Movies.Add(movie);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetMovie", new { id = movie.Id }, movie);
+        }
+
+        // DELETE: api/Movies/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMovie(Guid id)
+        {
+            var movie = await _context.Movies.FindAsync(id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            _context.Movies.Remove(movie);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool MovieExists(Guid id)
+        {
+            return _context.Movies.Any(e => e.Id == id);
         }
     }
 }
