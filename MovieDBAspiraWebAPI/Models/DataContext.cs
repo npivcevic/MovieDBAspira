7d4cba0091ca9b8c8518323b13a7109ce03fd029
Nikola Pivcevic <nikola.pivcevic@outlook.com>
2024-10-05 13:25:39 +0200

    Add DataContext

---
+++ MovieDBAspiraWebAPI/Models/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MovieDBAspiraWebAPI.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
